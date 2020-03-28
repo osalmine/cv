fbdbad4fcf8534923082b20e408cf1ed0af4114c
Onni Salminen <o.j.salminen@gmail.com>
2020-03-28 18:11:08 +0200

    Fixed the typing effect a tiny amount

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 var dataText = ["Student.", "Developer.", "Onni Salminen.", "PROJECTS"];
-function typeWriter(text, i, tag, fnCallback) {
+
+function typeWriter(text, i, tag, cur, max, fnCallback) {
 	if (i < (text.length)) {
 		document.querySelector(tag).innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true" class="anim"></span>';
 
 		setTimeout(function() {
-			typeWriter(text, i + 1, tag, fnCallback)
+			typeWriter(text, i + 1, tag, cur, max, fnCallback)
 		}, 120);
 	}
-	else if (typeof fnCallback == 'function') {
+	else if (typeof fnCallback == 'function' && cur != max - 1) {
+		console.log("Not last item, cur: " + cur + " max: " + max);
 		setTimeout(fnCallback, 700);
 	}
+	else if (typeof fnCallback == 'function' && cur == max - 1) {
+		console.log("Last item");
+		setTimeout(fnCallback, 2350);
+	}
 }
+
 function StartTextAnimation(i, max, tag) {
 	if (typeof dataText[i] == 'undefined' || i == max) {
 		document.querySelector(tag).innerHTML = dataText[max - 1];
 		return ;
 	}
 	if (i < dataText[i].length && i < max) {
-		typeWriter(dataText[i], 0, tag, function() {
+		typeWriter(dataText[i], 0, tag, i, max, function() {
 			StartTextAnimation(i + 1, max, tag);
 		});
 	}
@@ -30,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
 var isDone = 0;
 
 $(document).on('scroll', function() {
-    if($(this).scrollTop()>=$('#projects').position().top && isDone === 0) {
+    if ($(this).scrollTop()>=$('#projects').position().top && isDone === 0) {
 		StartTextAnimation(3, 4, '#prj');
 		isDone++;
     }
